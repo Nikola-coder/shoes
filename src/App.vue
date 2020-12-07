b2b335741489968fdc048762152b25a785ee4cd1
Nikola <nikola.tomasovic@medieinstitutet.se>
2020-12-07 14:48:13 +0100

    vue installationen

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+   <header>
+     <router-link to= "/"> <h1>Shoestore</h1> </router-link>
+   </header>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+</style>
